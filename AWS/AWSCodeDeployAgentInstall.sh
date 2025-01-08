9cd4328329acdd03f9f48058be7a5895682a2915
Gaurav Chauhan <145908403+Gaurav1517@users.noreply.github.com>
2025-01-08 16:31:42 +0530

    Update AWSCodeDeployAgentInstall.sh

--- AWS/AWSCodeDeployAgentInstall.sh
+++ AWS/AWSCodeDeployAgentInstall.sh
@@ -2,18 +2,13 @@
 # Ref: https://docs.aws.amazon.com/codedeploy/latest/userguide/codedeploy-agent-operations-install-linux.html
 
 #!/bin/bash
-# Update the system
 sudo yum update -y
-# Install dependencies
 sudo yum install ruby -y
 sudo yum install wget -y
-# Download and install CodeDeploy agent
-# Note: must change the AWS Region with working region..
+# Note: must change the AWS Region with current working region..
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
-# Start and enable CodeDeploy agent
 systemctl start codedeploy-agent
 systemctl enable codedeploy-agent
-# Check CodeDeploy agent status
 systemctl status codedeploy-agent
