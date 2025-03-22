b6d6b0c0023778a173c50bdc29cf047cf92a0fc8
Gaurav Chauhan <145908403+Gaurav1517@users.noreply.github.com>
2025-03-22 12:06:56 +0530

    Update tf-InstallUbuntu.sh

--- Terraform/tf-InstallUbuntu.sh
+++ Terraform/tf-InstallUbuntu.sh
@@ -4,14 +4,21 @@
 # Update your system and install required packages:
 sudo apt-get update && \
 sudo apt-get install -y gnupg software-properties-common
+
 # Install the HashiCorp GPG key:
-wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
 # Verify the key's fingerprint:
 gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
 #(Ensure the fingerprint matches the expected one. See the official guide for details.)
+
 #Add the HashiCorp repository:
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+
 # Update package information:
 sudo apt update
 # Install Terraform:
